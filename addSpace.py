0f865391bb626171ebb547b8a02dc515c71c97d1
Rajeswari979 <rajeswarim979@gmail.com>
2024-02-27 21:31:30 +0530

    program developed

--- addSpace.py
+++ addSpace.py
@@ -1,3 +1,9 @@
-hello
-helooo
-helooo
+str=input("enter the sentance")
+addSpace=""
+for i in range(0,len(str)):
+    if i % 2==0:
+        addSpace+=str[i]+" "
+    else:
+        addSpace+=str[i]
+
+print(addSpace)
