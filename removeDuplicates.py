af87cea962fd2d57b3b0002830f2b87ef1220af6
Rajeswari979 <rajeswarim979@gmail.com>
2024-02-27 20:09:16 +0530

    program created

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,1 +1,15 @@
-just try
+str=input("enter the string")
+standard=[]
+#first=str[0]
+for i in range(0,len(str)+1):
+    if str[i]==str[i+1]:
+        pass
+    else:
+        standard=str[i]
+
+print(standard)       
+   
+
+
+
+
