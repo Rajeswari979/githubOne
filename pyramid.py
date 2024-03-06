cd5f2ed025d82493c90a7a222803f81cdfc0c24a
Rajeswari979 <rajeswarim979@gmail.com>
2024-03-07 00:44:00 +0530

    26-02-24 Homeworks

--- pyramid.py
+++ pyramid.py
@@ -1,7 +1,12 @@
+#get a name from user
 name = input("enter the name : ")
 
+#using 2 forloop for develop pyramid
+#outer for loop for iterate name's length
 for i in range(0,len(name)):
+    #inner forloop for displaying character like a pyramid
     for j in range(0,i+1):
+        #using end=" " for avoid new line and display the character
         print(name[j] , end=" ")
-
+    
     print("\r")
