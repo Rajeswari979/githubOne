a7c6945d975b90fc9559602a9ef61b6bbfe5df62
Rajeswari979 <161428245+Rajeswari979@users.noreply.github.com>
2024-03-06 23:14:56 +0530

    26-02-24 Homework

--- addSpace.py
+++ addSpace.py
@@ -1,9 +1,20 @@
-str=input("enter the sentance : ")
-addSpace=""
+#add space in every 3rd place of the string
+
+#get string from user
+str=input("enter the sentance : ") 
+
+#declare a variable
+addSpace="" 
+
+#use for loop for getting single character from str
 for i in range(0,len(str)):
-    if i % 2==0:
-        addSpace+=" "+str[i]
+    #use % for calculate 3rd place and add space
+    if i % 2==0: #
+        addSpace+=" "+str[i] 
+
+    #if it is not a 3rd place ,then add next character in a str
     else:
         addSpace+=str[i]
 
+#finally , display the output
 print(addSpace)
