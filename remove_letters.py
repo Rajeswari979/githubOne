3d2466346b889f856b76cc5daaca9ee8e3261c94
Rajeswari979 <161428245+Rajeswari979@users.noreply.github.com>
2024-03-06 15:46:02 +0530

    finished

---
+++ remove_letters.py
@@ -0,0 +1,19 @@
+sentance=input("enter the string : ")
+str=sentance.split(' ')
+removeDuplicates=""
+new_sentance=""
+def func(word):
+   removeDuplicates=""
+   for letter in word:
+      if letter in removeDuplicates:
+          continue
+      else:
+         removeDuplicates+=letter
+   new_sentance=removeDuplicates
+   return new_sentance     
+         
+         
+
+for word in str:
+   print(func(word.lower()))
+       
