3f27398f161befbc6a2e91bf2db09a8ecf72c67f
Rajeswari979 <rajeswarim979@gmail.com>
2024-03-31 15:00:19 +0530

    29-03-24 HomeWork

---
+++ string_reverse.py
@@ -0,0 +1,11 @@
+#Reverse a string using stack
+#eg: input=Hello
+#output=olleH
+
+string="Hello"
+word=[]
+for i in string:
+    word.append(i)
+
+for j in range(-1,-len(word)-1,-1):
+    print(word[j],end=" ")    
